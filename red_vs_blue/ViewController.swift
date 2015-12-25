a522d68b9acb52f05359d54f82125d6a4fcfd53d
Jonny B <jonnyb@jonnys-macbook-pro.local>
2015-12-25 12:35:53 -0700

    Initial Commit

---
+++ red_vs_blue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  red_vs_blue
+//
+//  Created by Jonny B on 12/25/15.
+//  Copyright © 2015 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
